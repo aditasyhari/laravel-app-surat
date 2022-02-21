a9bf5e798af6863d61b604a2cb9a2f4081e6eb66
aditasyhari <adit.asyhari@gmail.com>
2022-02-22 01:29:43 +0700

    tipis tipis

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
-<!--
-Template Name: Materialize - Material Design Admin Template
-Author: PixInvent
-Website: http://www.pixinvent.com/
-Contact: hello@pixinvent.com
-Follow: www.twitter.com/pixinvents
-Like: www.facebook.com/pixinvents
-Purchase: https://themeforest.net/item/materialize-material-design-admin-template/11446068?ref=pixinvent
-Renew Support: https://themeforest.net/item/materialize-material-design-admin-template/11446068?ref=pixinvent
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
 
--->
 <html class="loading" lang="en" data-textdirection="ltr">
-<!-- BEGIN: Head-->
-
-<!-- Mirrored from pixinvent.com/materialize-material-design-admin-template/html/ltr/vertical-modern-menu-template/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Mar 2019 16:33:37 GMT -->
 
 <head>
     <title>
@@ -23,20 +9,14 @@ License: You must have a valid license purchased only from themeforest(the above
     @include('layout.style')
     @stack('style')
 </head>
-<!-- END: Head-->
 
 <body class="vertical-layout vertical-menu-collapsible page-header-dark vertical-modern-menu 2-columns  "
     data-open="click" data-menu="vertical-modern-menu" data-col="2-columns">
 
-    <!-- BEGIN: Header-->
     <header class="page-topbar" id="header">
         @include('layout.header')
     </header>
-    <!-- END: Header-->
 
-
-
-    <!-- BEGIN: SideNav-->
     <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-light sidenav-active-square">
         <div class="brand-sidebar">
             <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="index-2.html"><img
@@ -46,13 +26,10 @@ License: You must have a valid license purchased only from themeforest(the above
             @include('layout.sidenav')
         </div>
     </aside>
-    <!-- END: SideNav-->
 
-    <!-- BEGIN: Page Main-->
     <div id="main">
         @yield('content')
     </div>
-    <!-- END: Page Main-->
 
     <footer
         class="page-footer footer footer-static footer-dark gradient-45deg-indigo-purple gradient-shadow navbar-border navbar-shadow">
@@ -67,7 +44,5 @@ License: You must have a valid license purchased only from themeforest(the above
 
     @stack('js')
 </body>
-
-<!-- Mirrored from pixinvent.com/materialize-material-design-admin-template/html/ltr/vertical-modern-menu-template/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Mar 2019 16:34:49 GMT -->
 
 </html>
