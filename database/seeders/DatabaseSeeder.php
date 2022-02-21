20964d51660597ffa648db0282547dba0eabfe59
aditasyhari <adit.asyhari@gmail.com>
2022-02-21 23:30:42 +0700

    seeder awal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('user')->insert([
+            'nik' => '12345678',
+            'nama' => 'Ali Reza Nauri',
+            'email' => 'alireza@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin'
+        ]);
+
+        DB::table('user')->insert([
+            'nik' => '87654321',
+            'nama' => 'Agus Supono',
+            'email' => 'agussupono@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'user'
+        ]);
     }
 }
